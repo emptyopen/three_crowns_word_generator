836f2eeea93d27a7118d7d737a35b0c4aed964cf
Matt Takao <matt@codeparticle.com>
2019-10-05 15:11:20 -0700

    intial state

---
+++ android/app/src/main/java/com/takaomatt/three_crowns_word_generator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.takaomatt.three_crowns_word_generator;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
